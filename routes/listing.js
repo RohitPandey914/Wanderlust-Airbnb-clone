3970eafb78cedceafff34c5e19dbf12a3ae35df8
Rohit Pandey <roni914024@gmail.com>
2026-08-29 17:24:45 +0530

    implemented express-session and connect-flash

--- routes/listing.js
+++ routes/listing.js
@@ -46,6 +46,7 @@ router.get("/:id",wrapAsync(async(req,res)=>{
 router.post("/", validateListing, wrapAsync (async(req,res,next)=>{
      const newListing=new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success","New Listing Created!");
     res.redirect("/listings");
    
 }));
@@ -62,6 +63,7 @@ router.get("/:id/edit",wrapAsync(async(req,res)=>{
 router.put("/:id",validateListing,wrapAsync(async (req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
+     req.flash("success","Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -71,6 +73,7 @@ router.delete("/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+     req.flash("success","Listing Deleted!");
     res.redirect("/listings");
 
 }));
